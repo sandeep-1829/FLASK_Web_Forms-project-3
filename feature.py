d94d94cfe1a41f24162c8e40f1871ce5addb4bef
sandeep-1829 <godugunurusandeep@gmail.com>
2024-02-19 18:29:34 +0530

    first commit

---
+++ feature.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template,request
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+
+FAI=Flask(__name__)
+
+
+@FAI.route('/htmlforms',methods=['GET','POST'])
+def htmlforms():
+    if request.method=='POST':
+        FD=request.form
+        un=FD['un']
+        return FD['un']
+    return render_template('htmlforms.html')
+
+
+class NameForm(Form):
+    name=StringField()
+    submit=SubmitField()
+
+
+@FAI.route('/webforms',methods=['GET','POST'])
+def webforms():
+    NFO=NameForm()
+
+    if request.method=='POST':
+        NFDO=NameForm(request.form)
+        if NFDO.validate():
+            return NFDO.name.data
+    
+    return render_template('webforms.html',NFO=NFO)
+
+
+
+
+if __name__=='__main__':
+    FAI.run(debug=True)
+
+# if __name__=='__main__':
+#     FAI.run(debug=True,host='192.168.13.161',port=3000)
+    
+
+
+# import socket
+# hn=socket.gethostname()
+# ip=socket.gethostbyname(hn)
+# ip
+# '192.168.13.161'
